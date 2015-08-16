3498f6c89b7051818aec683859c51eaaa09debbf
Christopher Wood <christopherwood07@gmail.com>
2015-08-16 11:50:45 -0700

    Basic triangle shape

---
+++ src/Triangle.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+
+public class Triangle {
+    Vertex v1;
+    Vertex v2;
+    Vertex v3;
+    Color color;
+
+    public Triangle(Vertex vertex1, Vertex vertex2, Vertex vertex2, Color color) {
+        v1 = vertex1;
+        v2 = vertex2;
+        v3 = vertex3;
+        this.color = color;
+    }
+}
