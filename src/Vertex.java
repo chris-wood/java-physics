ce307d3920d61223ebdae21afbf26c4a8ddccf55
Christopher Wood <christopherwood07@gmail.com>
2015-08-16 11:49:10 -0700

    Vertex

---
+++ src/Vertex.java
@@ -0,0 +1,11 @@
+public class Vertex {
+    double x;
+    double y;
+    double z;
+
+    public Vertex(double xx, double yy, double zz) {
+        x = xx;
+        y = yy;
+        z = zz;
+    }
+}
