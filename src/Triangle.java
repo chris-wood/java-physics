66b39b813b3c4c7065967f813b059dd84706cfa9
Christopher Wood <christopherwood07@gmail.com>
2015-08-16 12:00:15 -0700

    Added draw logic

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,6 +1,6 @@
 import java.awt.Color;
 
-public class Triangle {
+public class Triangle implements Shape2D {
     Vertex v1;
     Vertex v2;
     Vertex v3;
@@ -11,5 +11,14 @@ public class Triangle {
         v2 = vertex2;
         v3 = vertex3;
         this.color = color;
+    }
+
+    public void draw(Graphics2D graphics) {
+        Path2D path = new Path2D.Double();
+        path.moveTo(v1.x, v1.y);
+        path.lineTo(v2.x, v2.y);
+        path.lineTo(v3.x, v3.y);
+        path.closePath();
+        graphics.draw(path);
     }
 }
